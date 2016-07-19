23d5cbbc78c453b2a99b411b0389f5bf900b42c2
mr.god1133 <mr.god1133@gmail.com>
2016-07-19 17:48:45 +0300

    profile page + Angular

---
+++ profile_page/js/controller/profile-contrl.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('testApp').controller('ProfileController', ProfileController);
+
+ProfileController.$inject = ['$scope', '$http'];
+
+function ProfileController($scope, $http) {
+
+	$scope.userName = "";
+	$scope.userSubname = "";
+	$scope.userBirthday = "";
+	$scope.userAvatar = "";
+	$scope.userContacts = [];
+	$scope.userCity = "";
+	$scope.userAbout = "";
+
+	$http.get('http://www.mocky.io/v2/578e33a20f0000ce00e9a041').success(success);
+
+	function success(data, status, headers, config){
+		if(status == 200)
+		{
+			$scope.userName = data.userName;
+			$scope.userSubname = data.userSubname;
+			$scope.userBirthday = data.userBirthday;
+			$scope.userAvatar = data.userAvatar;
+			$scope.userContacts = data.userContacts;
+			$scope.userCity = data.userCity;
+			$scope.userAbout = data.userAbout;
+		}
+	};
+};
+angular.module('testApp').controller('PostController', PostController);
+
+PostController.$inject = ['$scope', '$http'];
+
+function PostController($scope, $http) {
+
+	$scope.posts = [];
+
+	$http.get('http://www.mocky.io/v2/578e18f50f00006f19aebc38').success(success);
+
+	function success(data, status, headers, config){
+		if(status == 200)
+		{
+			$scope.posts = data;
+		}
+	};
+};
