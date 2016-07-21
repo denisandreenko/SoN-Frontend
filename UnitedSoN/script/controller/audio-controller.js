75ecbce3d9ed31bbf55a13c7fb1e505c074b27d4
Denis Andreenko <anreenkodn@gmail.com>
2016-07-21 15:35:05 +0300

    dopilil edit_profile - audio. add angular for groups and audio. united css

---
+++ UnitedSoN/script/controller/audio-controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('testApp').controller('AudioController', AudioController);
+
+AudioController.$inject = ['$scope', '$http'];
+
+function AudioController($scope, $http) {
+
+    $scope.audios = [];
+    $scope.code = "";
+    $scope.playIconUrl = "";
+
+    $http.get('http://www.mocky.io/v2/578f6a2b0f00002018e9a1d7').success(success);
+
+    function success(data, status, headers, config) {
+        $scope.audios = data.audios;
+        $scope.playIconUrl = data.playIcon;
+        $scope.code = data.code;
+    };
+    function doPlay() {
+        alert('PlayButton clicked');
+    };
+};
+angular.module('testApp').filter('trusted', AudioFilter);
+
+AudioFilter.$inject = ['$sce'];
+
+function AudioFilter($sce) {
+    return function (url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+};
--- UnitedSoN/script/controller/profile-controller.js
+++ UnitedSoN/script/controller/profile-controller.js
@@ -29,20 +29,3 @@ function ProfileController($scope, $http) {
 		}
 	};
 };
-angular.module('testApp').controller('PostController', PostController);
-
-PostController.$inject = ['$scope', '$http'];
-
-function PostController($scope, $http) {
-
-	$scope.posts = [];
-
-	$http.get('http://www.mocky.io/v2/578e18f50f00006f19aebc38').success(success);
-
-	function success(data, status, headers, config){
-		if(status == 200)
-		{
-			$scope.posts = data;
-		}
-	};
-};
