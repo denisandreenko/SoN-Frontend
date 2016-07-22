7c45b7cd811737fdf2e57c13a4084300cbb3c47d
mr.god1133 <mr.god1133@gmail.com>
2016-07-22 18:19:13 +0300

    123

--- social-network/app/script/app.js
+++ social-network/app/script/app.js
@@ -3,6 +3,19 @@
 var app = angular.module('socialNetwork', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-   
-    var i = 1;
+   $urlRouterProvider.otherwise('/id/profile');
+
+    $stateProvider
+        .state('menu', {
+            url: '/id',
+            templateUrl: 'view/menu.html'
+        })
+        .state('menu.groups',{
+            url: '/groups',
+            templateUrl: 'view/groups.html'
+        })
+        .state('menu.profile',{
+            url: '/profile',
+            templateUrl: 'view/profile.html'
+        })
 });
