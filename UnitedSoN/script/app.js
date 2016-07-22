adb85ebd59ad7129647dd6ef63aa04d9bc92129c
mr.god1133 <mr.god1133@gmail.com>
2016-07-22 17:28:26 +0300

    123

--- UnitedSoN/script/app.js
+++ UnitedSoN/script/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
-var testApp = angular.module('testApp', []);
+var testApp = angular.module('testApp', ["ui.router"]);
+
+
+testApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "html/home.html"
+        })
+        .state('audio', {
+            url: "/audio",
+            templateUrl: "html/audio.html"
+        })
+}]);
---
+++ social-network/app/script/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var app = angular.module('socialNetwork', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+   
+    var i = 1;
+});
