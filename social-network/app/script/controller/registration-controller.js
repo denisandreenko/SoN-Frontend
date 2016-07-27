030fe361c8c17b46f51f0cffd25055bf2307b394
mr.god1133 <mr.god1133@gmail.com>
2016-07-27 11:03:43 +0300

    reg

--- social-network/app/script/controller/registration-controller.js
+++ social-network/app/script/controller/registration-controller.js
@@ -1,3 +1,32 @@
-/**
- * Created by User-Book on 26.07.2016.
- */
+'use strict';
+
+angular.module('socialNetwork').controller('RegController', RegController);
+
+RegController.$inject = ['$scope', '$http'];
+
+function RegController($scope, $http) {
+
+    $scope.master = {};
+
+    $scope.update = function (user) {
+        $scope.master = angular.copy(user);
+
+        var req = {
+            method: 'POST',
+            url: 'http://private-bc396-authorisation.apiary-mock.com/oauth/token',
+            headers: {
+                'Content-Type': undefined
+            },
+            data: $scope.master
+        };
+
+
+        $http(req).success(success);
+        function success(data) {
+            alert('' + data.message);
+        };
+
+    };
+
+
+};
