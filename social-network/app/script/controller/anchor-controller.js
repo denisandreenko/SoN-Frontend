91d5d7e98ec0276a2182b7a98ed46cb55531181f
Denis Andreenko <anreenkodn@gmail.com>
2016-08-25 15:57:54 +0300

    fix registration form and color likes

--- social-network/app/script/controller/anchor-controller.js
+++ social-network/app/script/controller/anchor-controller.js
@@ -6,10 +6,6 @@ function ScrollController($scope, $location, $anchorScroll) {
 
 
     $scope.gotoTop = function() {
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('top');
-
       // call $anchorScroll()
       $anchorScroll();
     };
